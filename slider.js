bbcfa995e741f716d6ff980e171a6fc421ed33b4
Vaibhavpandey8 <96644799+Vaibhavpandey8@users.noreply.github.com>
2024-06-11 21:46:58 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(step) {
+    showSlide(currentSlide+step);
+    }
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        showSlide(currentSlide);
+    });
+    
